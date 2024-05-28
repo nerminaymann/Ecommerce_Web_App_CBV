95e16e513165086c049f70d1f3369eb99a880c0c
Nermin Ayman <45697519+nerminaymann@users.noreply.github.com>
2024-05-29 02:33:08 +0300

    prepare for testing with pytest

--- product/serializers.py
+++ product/serializers.py
@@ -1,19 +1,24 @@
 from rest_framework import serializers
 from .models import Product, Category, Brand
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['name']
 
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
+        fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    brand=BrandSerializer()
+    category=CategorySerializer()
+    class Meta:
+        model = Product
         fields = '__all__'
 
 
